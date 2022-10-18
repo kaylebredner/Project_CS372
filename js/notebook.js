7b78a71b194eeab4c2f8a9c7a2bdcddfba4d1b48
Tarun Jeevan <tjeevan200@gmail.com>
2022-10-18 12:46:50 -0400

    Add Notebook functionality complete
    Still need to figure out the display part though

--- js/notebook.js
+++ js/notebook.js
@@ -5,15 +5,15 @@ function addNotebook() {
 }
 
 function createNewNotebook() {
-    // Create the structure of the Notebook, which is just an <a>
-    var a = document.createElement('a');
+    // Create the structure of the Notebook, which is just a <div>
+    var a = document.createElement("a");
 
-    // Set up the new <a> classes then add the text
+    // Set up the new <div> classes then add the text
     a.className = "list-group-item list-group-item-action";
     a.id = "notebook";
-    a.onclick = "addNotebook()";
+    // a.onclick = "showNotebook()"; Displays notebook contents in right-side display
     a.append(document.createTextNode("New Notebook"));
 
-    // Return the <a>
+    // Return the <div>
     return a;
 }
