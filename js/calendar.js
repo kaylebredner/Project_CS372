17ce689c5297df4f4dd6335b17bb6747843001b5
Maxwell Maitland <mmdawg1@aol.com>
2022-11-16 13:48:01 -0500

    Front-end adding, Option to delete

--- js/calendar.js
+++ js/calendar.js
@@ -7,6 +7,8 @@ function init() {
        elementClicked = e
     })
 
+    fillNoteList()
+
     document.addEventListener('DOMContentLoaded',
         function () {
             var containerEl = document.getElementById('external-events')
@@ -42,7 +44,7 @@ function init() {
                 },
                 eventClick: function (info) {
                     info.jsEvent.preventDefault()
-                    calendarMenu(info)
+                    openContextMenu(info, "calendar")
                 }
             })
 
@@ -52,39 +54,58 @@ function init() {
 
 function appendEvent() {
     var eventDiv = document.getElementById("external-events")
-    eventDiv.append(createNewEventDiv())
-    return false
+    var selector = document.getElementById("add-event-menu")
+
+    
+    eventDiv.append(createNewEventDiv(selector[selector.selectedIndex]))
 }
 
-function createNewEventDiv() {
-    var outerDiv = document.createElement('div')
-    var innerDiv = document.createElement('div')
+function createNewEventDiv(option) {
+    let outerDiv = document.createElement('div')
+    let innerDiv = document.createElement('div')
+    let image = document.createElement('img')
+    let noteName = option.text
 
+    console.log(option.value)
+    if(option.value === "-1"){
+        console.log("new")
+        noteName = prompt("Please enter a name for your new note.")
+        alert("A blank note has been created for you!")
+        createNewNote(noteName)
+    }
+
+    image.id = "option-img"
+    image.alt = "remove event button"
+    image.src = "../images/trashcan.png"
+    image.addEventListener('click', function () {
+        removeCalendarNote(image)
+    })
 
     innerDiv.className = "fc-event-main"
-    innerDiv.id = "eventelement"
-    innerDiv.append(document.createTextNode("My event"))
+    innerDiv.id = "event-element"
+    innerDiv.append(document.createTextNode(noteName))
+    
     outerDiv.className = "fc-event fc-h-event fc-daygrid-event fc-daygrid-block-event"
 
     outerDiv.append(innerDiv)
+    outerDiv.append(image)
     return outerDiv
 }
 
-function completeEvent(info, element) {
-
+function completeEvent(info) {
     if (!info.event.classNames.includes("completed")) {
         info.event.setProp("classNames", info.event.classNames.concat(['completed']))
     }
     return false
 }
 
-function deleteEvent(info, element) {
+function deleteEvent(info) {
     info.event.remove()
     return false
 }
 
 function removeEventMenu(element) {
-    var child = element.lastElementChild
+    let child = element.lastElementChild
     while (child) {
         element.removeChild(child)
         child = element.lastElementChild
@@ -94,9 +115,8 @@ function removeEventMenu(element) {
     element.remove()
 }
 
-function openContextMenu(menuName) {
+function openContextMenu(info, menuName) {
     let element = document.elementFromPoint(elementClicked.clientX, elementClicked.clientY)
-    let menu = getMenuInfo()
 
     element = element.parentElement
     
@@ -104,43 +124,48 @@ function openContextMenu(menuName) {
 
         element.classList.add("menuEnabled")
 
+        //create container div for the menu
         let main = document.createElement('div')
-        let pcom = document.createElement('div')
-        let pdel = document.createElement('div')
-        let canc = document.createElement('div')
-
         main.classList.add("contextMenu")
-        pdel.classList.add("menuItem")
-        pcom.classList.add("menuItem")
+        element.append(main)
+
+        //create the close option for every context menu
+        let canc = document.createElement('div')
         canc.classList.add("menuItem")
         canc.classList.add("menuItem-close")
-        
-        pdel.innerText = "New Note"
-        pcom.innerText = "Existing Note"
         canc.innerText = "Close"
-
-        pdel.addEventListener('click', function () {
-        
-        })
-
-        pcom.addEventListener('click', function () {
-
-        })
-
         canc.addEventListener('click', function () {
             removeEventMenu(main)
         })
 
-        main.append(pcom)
-        main.append(pdel)
-        main.append(canc)
+        //build context menu
+        let menuItems = getMenuInfo(menuName, info)
+        for(let i = 0; i < menuItems.numItems; i++){
+            let item = document.createElement('div')
 
-        element.append(main)
-        console.log("done")
+            if(menuItems.useParent(item)){
+                item = item.parentElement
+            }
+
+            for(let j = 0; j < menuItems.numClassNames; j++){
+                item.classList.add(menuItems.className[j])
+            }
+
+            item.classList.add("menuItem")
+            item.innerText = menuItems.itemNames[i]
+            item.addEventListener('click', function(){
+                menuItems.functionNames[i](info)
+                removeEventMenu(main)
+            })
+
+            main.append(item)
+        }
+        
+        main.append(canc)
     }
 }
 
-function getMenuInfo(menuName){
+function getMenuInfo(menuName, info){
     const menu = {
         numItems: 0,
         numClassNames: 0,
@@ -151,11 +176,11 @@ function getMenuInfo(menuName){
     }
 
     switch(menuName){
-        case calendar:
+        case "calendar":
             menu.numItems = 2
             menu.numClassNames = 0
             menu.itemNames = ["Complete", "Delete"]
-            menu.functionNames = [completeEvent(), deleteEvent()]
+            menu.functionNames = [function(){completeEvent(info)}, function(){deleteEvent(info)}]
             menu.useParent = function(elem){
                 if (elem.classList.contains("fc-event-title")) {
                     return true
@@ -164,18 +189,27 @@ function getMenuInfo(menuName){
                 return false
             }
             break;
-        case eventAdd:
-            menu.numItems = 2
-            menu.numClassNames = 1
-            menu.itemNames = ["addNewEvent", "addExistingEvent"]
-            menu.classNames = ["addEventMenu"]
-            menu.functionNames = [addNewEvent(), addExistingEvent()]
-            menu.useParent = function(elem){return true}
-            break;
-        case eventRemove:
-
-            break;
     }
 
     return menu
+}
+
+function fillNoteList(){
+    let allNotes = null//fetch list of notes
+    let selector = document.getElementById("add-event-menu")
+
+    for(let i = 0; i < allNotes; i++){
+        let option = document.createElement("option")
+        option.value = null//grab note ID from DB
+        option.text = null//grab note name from DB
+        selector.append(option)
+    }
+}
+
+function createNewNote(){
+    //add a note to the db for this user
+}
+
+function removeCalendarNote(elem){
+    removeEventMenu(elem.parentElement)
 }
