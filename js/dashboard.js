7aabf5c4d2e9f340d1fe32876ed9d3bd9d1728b0
Alex Bishop <abishop@ntserv.doitbestcorp.com>
2022-12-04 17:20:18 -0500

    Revamped dashboard, added user analytics

--- js/dashboard.js
+++ js/dashboard.js
@@ -2,16 +2,39 @@ function getData() {
     const username = "alex"
     $.ajax({
         url: '/eventsAmount/' + username,
-        success: function(data) {
+        success: function (data) {
             const finalString = `You have ${data} events scheduled!`
-            document.getElementById('welcomeUsername').innerHTML=`Welcome ${username}!`
-            document.getElementById('eventString').innerHTML=finalString
+            console.log(finalString)
+            document.getElementById('eventString').innerHTML = finalString
         }
     })
 } // end getData
-
-function eventsNav() {
+function navCalandar() {
+    const user = sessionStorage.getItem('planner-username')
     $.ajax({
-        url: '/calendar'
+        url: '/calendar/' + user,
+        success: function (data) {
+            window.location.href = '/calendar/' + user
+        },
+        statusCode: {
+            401: function (data) {
+                window.location.href = '/'
+            }
+        }
+    })
+}
+
+function navNotes() {
+    const user = sessionStorage.getItem('planner-username')
+    $.ajax({
+        url: '/calendar/' + user,
+        success: function (data) {
+            window.location.href = '/notes/' + user
+        },
+        statusCode: {
+            401: function (data) {
+                window.location.href = '/'
+            }
+        }
     })
 }
