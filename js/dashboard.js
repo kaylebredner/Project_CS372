cad446b6872cddf280b137f026382e7e9b880a1e
Alex Bishop <abishop@ntserv.doitbestcorp.com>
2022-12-04 16:39:45 -0500

    push

--- js/dashboard.js
+++ js/dashboard.js
@@ -4,8 +4,14 @@ function getData() {
         url: '/eventsAmount/' + username,
         success: function(data) {
             const finalString = `You have ${data} events scheduled!`
-            console.log(finalString)
+            document.getElementById('welcomeUsername').innerHTML=`Welcome ${username}!`
             document.getElementById('eventString').innerHTML=finalString
         }
     })
 } // end getData
+
+function eventsNav() {
+    $.ajax({
+        url: '/calendar'
+    })
+}
